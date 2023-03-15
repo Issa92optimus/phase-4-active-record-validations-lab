9cb1392083f448387ddca745d857298adf41a359
Issa Nyale <me@email.com>
2023-03-15 12:14:52 +0300

    completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :title_is_sufficiently_clickbaity
+
+    def title_is_sufficiently_clickbaity
+        if title
+            unless title.include?("Won't Believe") || title.include?("Secret") || title.match?(/Top\s\d/) || title.include?("Guess")
+            errors.add(:title, "title is not sufficiently clickbait-y")
+            end
+        end
+    end
 end
